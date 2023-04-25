f42c1576e964f6ea16f4708e6357d515e810a228
Bhaskar Sharma <imbhaskaran@gmail.com>
2023-04-25 23:26:20 +0530

    remove jwt secret

--- config/config.js
+++ config/config.js
@@ -4,13 +4,10 @@ require("dotenv").config();
 const APP_PORT = process.env.APP_PORT || "5000";
 
 const config = {
-  APP_HOST: "0.0.0.0",
-  APP_PORT: APP_PORT,
-  JWT_SECRET: "f271e206119433acadc4hd",
   development: {
     APP_HOST: "0.0.0.0",
     APP_PORT: APP_PORT,
-    JWT_SECRET: "f271e206119433acadc4hd",
+    JWT_SECRET: process.env.JWT_SECRET,
     DB_URI: `postgres://postgres:secret@elocalhost:5432/postgres`,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
