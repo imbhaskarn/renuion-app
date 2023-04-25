71cf6acf91009efe5ba0969f75060c805be3b9c0
Bhaskar Sharma <imbhaskaran@gmail.com>
2023-04-25 23:23:08 +0530

    database connection and sequelize working

--- config/config.js
+++ config/config.js
@@ -1,27 +1,24 @@
-import dotenv from "dotenv";
-dotenv.config();
+"use strict";
 
-const env = process.env.NODE_ENV || "development";
-console.log(env)
-const db = {
-  production: {
-    username: "root",
-    password: null,
-    database: "production",
-    host: "127.0.0.1",
-    dialect: "postgres",
-  },
-  development: {
-    username: "postgres",
-    password: "secret",
-    database: "postgres",
-    host: "127.0.0.1",
-    dialect: "postgres",
-  },
-};
+require("dotenv").config();
+const APP_PORT = process.env.APP_PORT || "5000";
 
 const config = {
-  db: db[env],
+  APP_HOST: "0.0.0.0",
+  APP_PORT: APP_PORT,
+  JWT_SECRET: "f271e206119433acadc4hd",
+  development: {
+    APP_HOST: "0.0.0.0",
+    APP_PORT: APP_PORT,
+    JWT_SECRET: "f271e206119433acadc4hd",
+    DB_URI: `postgres://postgres:secret@elocalhost:5432/postgres`,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+  },
 };
-console.log(config.db)
-export default config;
+// console.log(config)
+module.exports = config;
